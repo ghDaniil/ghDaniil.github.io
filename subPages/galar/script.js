70f90007835edaa1ca9c261c150b605d558a5067
ghDaniil <ghDaniilPro@yandex.russasdas>
2020-10-31 15:53:25 +0300

    danil danil daniilov

--- subPages/galar/script.js
+++ subPages/galar/script.js
@@ -2,8 +2,13 @@ $('.carousel').carousel({
     interval: 550 
 })
 
-
 $(".img_item").click(function (e) { 
-    e.preventDefault();
+    console.log($(".img_item"));
+    let imgArr = $(".img_item");
+    for (let i = 0; i < imgArr.length; i++) {
+        if (imgArr[i] ==e.target) {
+            $('.carousel').carousel(i)
+        }
+    }
     
 });
