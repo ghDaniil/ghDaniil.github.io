406bfdf82c4e9ea988e8dfb32ca4970093e049e6
ghDaniil <ghDaniilPro@yandex.russasdas>
2020-12-05 15:59:38 +0300

    danil_you_daniil

---
+++ script.js
@@ -0,0 +1,45 @@
+
+$('#recipeCarousel').carousel({
+    interval: 10000
+})
+  
+$('.carousel .carousel-item').each(function(){
+    var minPerSlide = 3;
+    var next = $(this).next();
+    if (!next.length) {
+    next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+    
+    for (var i=0;i<minPerSlide;i++) {
+        next=next.next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
+        }
+        
+        next.children(':first-child').clone().appendTo($(this));
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $("body").click(function (e) { 
+//     if(e.target.id == "up"){
+//         alert("dd")
+//     }
+    
+// });
